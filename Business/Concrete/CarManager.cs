b63a759589c960f81c7c9f0cf0fd6f1454cdac45
smhbc <smhbc@SEMIHBICAK>
2024-03-21 15:24:57 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll() // İş sınıfı başka sınıfları new'lemez.
+        {
+            return _carDal.GetAll(); 
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars; // Class içinde ama methodların dışında olduğu için global değişkendir. "_" ile başlar değişken. Naming commention
+        public InMemoryCarDal()
+        {
+            // Veritabanından geliyor bu veriler.
+            _cars = new List<Car> {
+                new Car {Id =1,BrandId=1,ColorId=1,DailyPrice=2500,Description="Passat",ModelYear=2023},
+                new Car {Id =2,BrandId=1,ColorId=3,DailyPrice=2500,Description="Golf",ModelYear=2021},
+                new Car {Id =3,BrandId=2,ColorId=1,DailyPrice=2500,Description="Ford Focus",ModelYear=2020},
+                new Car {Id =4,BrandId=2,ColorId=1,DailyPrice=2500,Description="Ford Connect",ModelYear=2022},
+                new Car {Id =5,BrandId=3,ColorId=3,DailyPrice=2500,Description="Honda",ModelYear=2024}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete =_cars.SingleOrDefault(c=>c.Id == car.Id); // SingleOrDefault(c=>) bu kod foreach gibi listeyi tek tek dolaşmaya yarar.
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars; //Veritabanının tümünü döndürüyor
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c=> c.Id == carId).ToList();  // Şarta uyan bütün elemanları yeni liste haline getirip oluşturur.
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id); // Güncellemek istediğin veriler tabloda bulunup,güncellenir.
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;  
+        }
+    }
+}
