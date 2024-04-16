dd51217521223ea6ed706b5089dda315f412be44
smhbc <smhbc@SEMIHBICAK>
2024-04-16 12:36:11 +0300

    ConsoleUI fixed

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 
@@ -10,23 +11,53 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+
+            //BrandTest();
+
+            //ColorTest();
+
+            RentalsTest();
+        }
+
+        private static void RentalsTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            Rental rental = new Rental { CarId = 5, CustomerId = 4, RentDate = DateTime.Now };
+            rentalManager.Add(rental);
+
+            foreach (var Rental in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine($"{Rental.Id} / {Rental.CarId} / {Rental.CustomerId} / {Rental.RentDate} / {Rental.ReturnDate?.ToString()}");
+            }
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            foreach (var color in colorManager.GetAll().Data)
+            {
+                Console.WriteLine($"{color.ColorName}");
+            }
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
 
-            foreach (var car in carManager.GetCarDetails())
+            foreach (var car in carManager.GetCarDetails().Data)
             {
                 Console.WriteLine($"Açıklama:{car.CarName} Marka:{car.BrandName} Renk:{car.ColorName} Fiyat:{car.DailyPrice}");
 
             }
-
-            //BrandTest();
-
         }
 
         private static void BrandTest()
         {
             BrandManager brandManager = new BrandManager(new EfBrandDal());
 
-            foreach (var brand in brandManager.GetAll())
+            foreach (var brand in brandManager.GetAll().Data)
             {
                 Console.WriteLine(brand.BrandName);
             }
